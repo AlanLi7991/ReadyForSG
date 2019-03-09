b4f4bb98066556245947e2a6c3a0a6cd224661e3
zhengsenyao <zhengsy_bupt@163.com>
2019-03-09 12:42:11 +0800

    补充runloop的相关知识

--- ReadyForSG/07Runloop/ConceptAndLifeCycle/SGRunloop.m
+++ ReadyForSG/07Runloop/ConceptAndLifeCycle/SGRunloop.m
@@ -23,12 +23,17 @@
  * while循环，只不过这个while循环，一是可以在线程不使用时暂时休眠，不占用
  * 系统资源，这是依靠系统内核实现的；二是可以接受各种事件来唤醒线程。
  *
+ * 苹果官网文档：
+ * https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/Multithreading/RunLoopManagement/RunLoopManagement.html#//apple_ref/doc/uid/10000057i-CH16-SW23
+ *
  * 一篇讲得比较全面的文章：
  * http://weslyxl.coding.me/2018/03/18/2018/3/RunLoop从源码到应用全面解析/
  *
  * Mark:
- * Runloop有C、OC和swift三套API，C语言的API是线程安全的，OC的API是非线程安全的。
+ * 1. Runloop有C、OC和swift三套API，C语言的API是线程安全的，OC的API是非线程安全的。
  * 使用OC接口在某个线程中使用另一个线程的NSRunloop对象，可能出现奇怪的现象。
+ * 2. Runloop不能自己创建，因为它跟线程绑定，可以在线程中获取到它。
+ * 3. App启动时，主线程就处于Runloop中。其他线程需要其他启动Runloop
  */
 
 //----------------------------------------------------------------------------//
@@ -52,3 +57,81 @@
  *    候，要用NSRunLoopCommonModes作为参数的原因
  * 4. GSEventReceiveRunLoopMode：用于接收触摸、点击等事件以唤醒runloop的
  */
+
+
+//----------------------------------------------------------------------------//
+#pragma mark - Runloop Source
+//----------------------------------------------------------------------------//
+/**
+ * Runloop的Source可以自定义，苹果默认定义了两种实现，Source0和Source1。
+ * 1. Source0负责App内部事件，由App管理触发，如UITouch事件，这类Source需要手动触发唤醒Runloop
+ * 2. Source1是基于Port的，除了包含回调指针外还包含一个mach port。不同于Source0，
+ *    Source1可以监听系统端口和其他线程通信，它能够主动唤醒Runloop
+ */
+
+
+//----------------------------------------------------------------------------//
+#pragma mark - Runloop Observer Activities
+//----------------------------------------------------------------------------//
+/**
+     typedef CF_OPTIONS(CFOptionFlags, CFRunLoopActivity) {
+     kCFRunLoopEntry = (1UL << 0), // 进入RunLoop
+     kCFRunLoopBeforeTimers = (1UL << 1), // 即将开始Timer处理
+     kCFRunLoopBeforeSources = (1UL << 2), // 即将开始Source处理
+     kCFRunLoopBeforeWaiting = (1UL << 5), // 即将进入休眠
+     kCFRunLoopAfterWaiting = (1UL << 6), //从休眠状态唤醒
+     kCFRunLoopExit = (1UL << 7), //退出RunLoop
+     kCFRunLoopAllActivities = 0x0FFFFFFFU
+     };
+*/
+
+//----------------------------------------------------------------------------//
+#pragma mark - Runloop Timer
+//----------------------------------------------------------------------------//
+/**
+ * Runloop中的Timer，实际上其上层就是NSTimer。它跟Source1一样，也是基于Port的。
+ * 只不过，所有Timer公用一个Port，Mode Timer Port。因此，Timer不需要手动触发就能唤醒Runloop。
+ *
+ * Mark:
+ * 1. 使用NSTimer之前必须注册到Runloop中，但是RunLoop为了节省资源，
+ *    并不会在非常准确的时间点调用定时器，如果一个任务执行时间较长，那
+ *    么当错过一个时间点后只能等到下一个时间点执行
+ * 2. NSTimer有两种创建方式，timerWithXXX和scheduleTimerWithXXX，
+ *    两者的区别在于后者除了创建一个timer之外还会自动以kCFRunLoopDefaultMode的
+ *    mode添加到当前线程的runloop中
+ * 3. 使用scheduleTimerWithXXX的接口创建的timer，在屏幕滑动时，timer是不会正常工作的
+ */
+
+
+//----------------------------------------------------------------------------//
+#pragma mark - Runloop Cycle
+//----------------------------------------------------------------------------//
+/**
+ * 1. 通知Observer即将进入Runloop（kCFRunLoopEntry）
+ *                   Cycle Start
+ * ------------------------------------------------
+ * 2. 通知observer即将处理timer（kCFRunLoopBeforeTimers）
+ * 3. 通知observer即将处理source0（kCFRunLoopBeforeSources）
+ * 4. 处理非延迟的主线程调用
+ * 5. 处理source0事件
+ * 6. 判断是否存在source1，若存在source1，直接跳到第10步
+ * 7. 通知observer即将进入休眠状态（kCFRunLoopBeforeWaiting）
+ * 8. 有事件唤醒runloop，如source0手动触发、source1主动唤醒、
+      timer主动唤醒、dispatch_main_queue唤醒
+ *    a. An event arrives for a port-based input source.
+ *    b. A timer fires.
+ *    c. The timeout value set for the run loop expires.
+ *    d. The run loop is explicitly woken up.
+ * 9. 通知observer即将被唤醒（kCFRunLoopAfterWaiting）
+ * 10.
+ *    a. 若是timer唤醒，处理timer事件
+ *    b. 否则若是main dispatch唤醒，处理main dispatch
+ *    c. 否则处理source1事件
+ * 11. 若发生下面四个条件中的一条，则退出runloop（直接到12），否则跳到第2步
+ *    a. 进入loop时参数说明loop执行完就返回
+ *    b. 超出传入参数的超时时间了
+ *    c. 被外部调用者停掉了
+ *    d. 没有任何source、timer了（网上把observer也包括了，应该是错的）
+ * 12. 通知observer即将退出runloop（kCFRunLoopExit）
+ */
+
